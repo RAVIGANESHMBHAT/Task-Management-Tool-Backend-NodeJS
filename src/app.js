325f5055fb9c16092acebd2dd207047d2ae48ed4
I524764 <raviganesh.maniyoor@sap.com>
2022-06-07 18:23:23 +0530

    Handled cors issue

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const cors = require('cors');
 
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 //const taskRouter = require('./routers/task')
 
 const app = express()
+
+
+app.use(cors({
+    origin: 'https://772c8e76trial.launchpad.cfapps.us10.hana.ondemand.com/4efbd0e5-5068-48b0-a935-cbbed1e048b3.taskmanagementtool.taskmanagementtool-0.0.1'
+}));
 
 app.use(express.json())
 app.use(userRouter)
