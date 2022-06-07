a67fb265d51fcb12af632786c7198dae2b388edb
I524764 <raviganesh.maniyoor@sap.com>
2022-06-07 17:09:01 +0530

    Initial commit

---
+++ src/routers/task.js
@@ -0,0 +1,116 @@
+// const express = require('express')
+// const Task = require("../models/Task");
+// const auth = require('../middleware/auth')
+// const router = express.Router()
+
+// router.post('/tasks', auth, async (req, res) => {
+//     const task = new Task({
+//         ...req.body,
+//         owner: req.user._id
+//     })
+
+//     try {
+//         await task.save()
+//         res.status(201).send(task)
+//     } catch(e) {
+//         res.status(400).send(e)
+//     }
+// })
+
+// //GET /tasks?completed=false
+// //limit, skip (Used for pagination) eg: GET /tasks?limit=10&skip=0
+// //Sorting: GET /tasks?sortBy=createdAt:desc
+// router.get('/tasks', auth,  async (req, res) => {
+//     //const userId = req.user._id
+
+//     const match = {}
+//     const sort = {}
+
+//     if(req.query.completed) {
+//         match.completed = req.query.completed === "true"
+//     }
+
+//     if(req.query.sortBy) {
+//         const parts = req.query.sortBy.split(':')
+//         sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+//     }
+
+//     try {
+//         //const tasks = await Task.find({ owner: userId })
+//         await req.user.populate({
+//             path: 'tasks',
+//             match,
+//             options: {
+//                 limit: parseInt(req.query.limit),
+//                 skip: parseInt(req.query.skip),
+//                 sort
+//             }
+//         })
+//         res.status(200).send(req.user.tasks)
+//     } catch(e) {
+//         res.status(500).send(e)
+//     }
+// })
+
+// router.get('/tasks/:id',  auth, async (req, res) => {
+//     const taskId = req.params.id
+//     const userId = req.user._id
+//     try {
+//         //const task = await Task.findById(taskId)
+//         const task = await Task.findOne({taskId, owner: userId})
+
+//         if(!task) {
+//             res.status(404).send()
+//         }
+//         res.status(200).send(task)
+//     } catch(e) {
+//         res.status(500).send(e)
+//     }
+// })
+
+// router.patch('/tasks/:id', auth,  async (req, res) => {
+//     const updates = Object.keys(req.body)
+//     const allowedUpdates = ['description', 'completed']
+//     const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+
+//     const userId = req.user._id
+
+//     if(!isValidOperation) {
+//         return res.status(400).send({ error: 'Invalid updates'})
+//     }
+
+//     try {
+//         //const task = await Task.findById(req.params.id)
+//         const task = await Task.findOne({ _id: req.params.id, owner: userId})
+
+//         if(!task) {
+//             return res.status(404).send()
+//         }
+
+//         updates.forEach((update) => {
+//             task[update] = req.body[update]
+//         })
+//         await task.save()
+//         //const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
+
+//         return res.status(200).send(task)
+//     } catch(e) {
+//        return res.status(400).send(e)
+//     }
+// })
+
+// router.delete('/tasks/:id', auth, async (req, res) => {
+//     try {
+//         // const task = await Task.findByIdAndDelete(req.params.id)
+//         const task = await Task.findOneAndDelete({_id: req.params.id, owner: req.user._id})
+
+//         if(!task) {
+//             return res.status(404).send()
+//         }
+//         res.status(200).send(task)
+//     } catch(e) {
+//         res.status(500).send()
+//     }
+// })
+
+// module.exports = router
---
+++ src/routers/user.js
@@ -0,0 +1,160 @@
+const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
+
+const User = require("../models/User");
+// const auth = require('../middleware/auth')
+// const { sendWelcomeEmail, sendCancellationEmail } = require('../emails/account')
+
+const router = express.Router()
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+    console.log(user)
+
+    try {
+        await user.save()
+        //sendWelcomeEmail(user.email, user.name)
+        //const token = await user.generateAuthToken()
+        res.status(201).send(user)//send({user, token} )
+    } catch(e) {
+        res.status(400).send(e)
+    }
+})
+
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.status(200).send(users)
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
+
+/*router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.status(200).send({user, token})
+    } catch(e) {
+        res.status(401).send()
+    }
+})
+
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.send()
+    } catch(e) {
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch(e) {
+        res.status(500).send()
+    }
+})
+
+router.get('/users/me', auth, async (req, res) => {
+    res.status(200).send(req.user)
+})
+
+// router.get('/users/:id', async (req, res) => {
+//     const userId = req.params.id
+//     try {
+//         const user = await User.findById(userId)
+//         if(!user) {
+//             return res.status(404).send()
+//         }
+//         res.status(200).send(user)
+//     } catch(e) {
+//         res.status(500).send(e)
+//     }
+// })
+
+router.patch('/users/me', auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+
+    if(!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates'})
+    }
+
+    try {
+        updates.forEach((update) => {
+            req.user[update] = req.body[update]
+        })
+        await req.user.save()
+        res.status(200).send(req.user)
+    } catch(e) {
+        res.status(400).send(e)
+    }
+})
+
+router.delete('/users/me', auth, async (req, res) => {
+    try {
+        await req.user.remove()
+        //sendCancellationEmail(req.user.email, req.user.name)
+        res.status(200).send(req.user)
+    } catch(e) {
+        res.status(500).send()
+    }
+})
+
+const upload = multer({
+    // dest: 'avatars',
+    limits: {
+        fileSize: 1000000 //1 MB
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(jpg|jpeg|png)/)) {
+            return cb(new Error('File must be of either jpg, jpeg or png'))
+        }
+        return cb(null, true)
+
+        // cb(new Error('File must be a PDF'))
+        // cb(undefined, true)
+        // cb(undefined, false)
+    }
+})
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.status(200).send({ message: "Avatar deleted successfully"})
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById( req.params.id)
+
+        if(!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/png')
+        res.status(200).send(user.avatar)
+
+    } catch (e) {
+        res.status(404).send()
+    }
+})*/
+module.exports = router
