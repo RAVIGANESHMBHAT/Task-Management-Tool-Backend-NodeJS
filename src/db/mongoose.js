f172a116821e38542c213c9c2ad1deac62a11156
I524764 <raviganesh.maniyoor@sap.com>
2022-06-07 18:35:47 +0530

    CORS

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 
 const connectionURL = process.env.DATABASE_URL;
 
-mongoose.connect(connectionURL, { useNewUrlParser: true})
+mongoose.connect("mongodb+srv://raviganeshm:Computer2%40@cluster0.fvf2b.mongodb.net/task-manager-backend-dissertation?retryWrites=true&w=majority", { useNewUrlParser: true})
 
 
